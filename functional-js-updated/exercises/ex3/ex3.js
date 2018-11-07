2e955fa8a4d91658528e5a88ebe9b0da9be846bf
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2018-11-07 17:31:36 +0100

    challenge 3 done

--- functional-js-updated/exercises/ex3/ex3.js
+++ functional-js-updated/exercises/ex3/ex3.js
@@ -1,16 +1,47 @@
-function increment(x) { return x + 1; }
-function decrement(x) { return x - 1; }
-function double(x) { return x * 2; }
-function half(x) { return x / 2; }
+function increment(x) {
+  return x + 1;
+}
+function decrement(x) {
+  return x - 1;
+}
+function double(x) {
+  return x * 2;
+}
+function half(x) {
+  return x / 2;
+}
 
-function compose() {}
-function pipe() {}
+function compose(...args) {
+  return function(val) {
+    return [...args].reduceRight(function(prev, next) {
+      return next(prev);
+    }, val);
+  };
+}
+function pipe(...args) {
+  return function(val) {
+    return args.reduce(function(prev, next) {
+      return next(prev);
+    }, val);
+  };
+}
 
-var f = compose(decrement,double,increment,half);
-var p = pipe(half,increment,double,decrement);
+var f = compose(
+  decrement,
+  double,
+  increment,
+  half
+);
+var p = pipe(
+  half,
+  increment,
+  double,
+  decrement
+);
 
-f(3) === 4;
+console.log(f(3) === 4);
 // true
 
-f(3) === p(3);
-// true
+console.log(f(3));
+console.log(p(3));
+console.log(f(3) === p(3));
